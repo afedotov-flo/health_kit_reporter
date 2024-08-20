c0ff775f0bd2f5db86966839fdce3751f1e535a2
Tim <102026641+afedotov-flo@users.noreply.github.com>
2024-08-20 17:21:07 +0500

    fix electrocardiogram

--- lib/model/payload/electrocardiogram.dart
+++ lib/model/payload/electrocardiogram.dart
@@ -79,9 +79,9 @@ class ElectrocardiogramHarmonized {
     this.metadata,
   );
 
-  final num averageHeartRate;
+  final num? averageHeartRate;
   final String averageHeartRateUnit;
-  final num samplingFrequency;
+  final num? samplingFrequency;
   final String samplingFrequencyUnit;
   final String classification;
   final String symptomsStatus;
